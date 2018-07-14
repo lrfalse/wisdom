f5f772aef186ce37d0dc37666477e68fd4852331
yanyunfeng <fxcxoa@163.com>
2018-07-14 12:04:08 +0800

    添加终端测试类

---
+++ wisdom-housing-service/housing-service/src/main/java/com/wisdom/rpc/RpcClientTest.java
@@ -0,0 +1,12 @@
+package com.wisdom.rpc;
+
+import com.wisdom.third.famvideo.FamVideoClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RpcClientTest {
+    @Autowired
+    FamVideoClient famVideoClent;
+    
+}
